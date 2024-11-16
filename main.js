348ac2a30cca5b88f05d501893f24383e3d0d9c2
kasraalitabar <kasraalitabar@gmail.com>
2024-11-16 12:49:35 +0330

    mid

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const body = document.body;
-function darkthem(){
-    body.classList.add("dark")
-}
-function lightthem(){
-    body.classList.add("light")
+// function darkthem(){
+//     body.classList.add("dark")
+
+// }
+// function lightthem(){
+//     body.classList.add("light")
+// }
+function swich(){
+    body.classList.toggle("dark")
 }
